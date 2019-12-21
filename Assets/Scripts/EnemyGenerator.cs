6a305740b5070520a176d08c61c47b016cec829c
LuisCarranza <luiscarranza1995@gmail.com>
2019-12-20 20:57:12 -0600

    Cámara en tercera persona, animaciones, spawn de enemigos básico

---
+++ Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Vector2 spawnRange;
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", 0f, 3.0f);
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab,
+                    new Vector3(
+                        Random.Range(-spawnRange.x, spawnRange.x),
+                        0,
+                        Random.Range(-spawnRange.y, spawnRange.y)),
+                    Quaternion.identity);
+    }
+}
