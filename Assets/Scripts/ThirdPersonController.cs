4a8583b0bde050a7d9d7b91ce6c448e942696792
LuisCarranza <luiscarranza1995@gmail.com>
2020-01-11 13:34:12 -0600

    Add Mobile Control

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -8,6 +8,9 @@ public class ThirdPersonController : MonoBehaviour
     public Transform objectToGrab;
     private AnimatorStateInfo currentState;
     private CapsuleCollider capsule;
+    private Touch fingerTouch;
+    private Vector2 posInit;
+    private Vector2 swipeDirection;
     void Start()
     {
         playerAnimator = GetComponent<Animator>();
@@ -17,8 +20,73 @@ public class ThirdPersonController : MonoBehaviour
     void Update()
     {
         currentState = playerAnimator.GetCurrentAnimatorStateInfo(0);
+#if UNITY_STANDALONE
         playerAnimator.SetFloat("Speed", Input.GetAxis("Vertical"));
         playerAnimator.SetFloat("Direction", Input.GetAxis("Horizontal"));
+#endif
+#if UNITY_ANDROID || UNITY_IOS
+#if !ACCEL_CONTROLLER && !SWIPE_CONTROLLER
+        playerAnimator.SetFloat("Speed", 1f);
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).position.x > (Screen.width * 0.5f))
+                playerAnimator.SetFloat("Direction", 1f);
+            else
+                playerAnimator.SetFloat("Direction", -1f);
+        }
+        else
+            playerAnimator.SetFloat("Direction", 0f);
+#endif
+#if ACCEL_CONTROLLER
+        playerAnimator.SetFloat("Direction", Input.acceleration.x);
+        if (Input.touchCount > 0)
+            playerAnimator.SetFloat("Speed", 1f);
+        else
+            playerAnimator.SetFloat("Speed", 0f);
+#endif
+#if SWIPE_CONTROLLER
+        playerAnimator.SetFloat("Direction", Input.acceleration.x);
+        if (Input.touchCount > 0)
+        {
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                fingerTouch = Input.GetTouch(i);
+                if (fingerTouch.position.x > (Screen.width * 0.5f))
+                {
+                    switch (fingerTouch.phase)
+                    {
+                        case TouchPhase.Ended:
+                        case TouchPhase.Canceled:
+                            playerAnimator.SetFloat("Speed", 0f);
+                            break;
+                        default:
+                            playerAnimator.SetFloat("Speed", 1f);
+                            break;
+                    }
+                }
+                else
+                {
+                    // Evaluar Swipe
+                    switch (fingerTouch.phase)
+                    {
+                        case TouchPhase.Began:
+                            posInit = fingerTouch.position;
+                            break;
+                        case TouchPhase.Ended:
+                            swipeDirection = fingerTouch.position - posInit;
+                            if (swipeDirection.magnitude < 10f)
+                                return;
+                            swipeDirection.Normalize();
+                            if (Mathf.Abs(Vector2.Angle(Vector2.up, swipeDirection)) < 15f)
+                                if (currentState.IsName("Locomotion"))
+                                    playerAnimator.SetTrigger("Jump");
+                            break;
+                    }
+                }
+            }
+        }
+#endif
+#endif
         if (Input.GetKeyDown(KeyCode.Space) && currentState.IsName("Locomotion"))
             playerAnimator.SetTrigger("Jump");
         if (Input.GetKeyDown(KeyCode.Q))
